9e01301818ecd7bb97e89cad1b8340f2b28da48f
徐桓 <765155143@qq.com>
2024-04-02 10:29:09 +0800

    chore: 修改modkey 和 rules

--- config.def.h
+++ config.def.h
@@ -33,9 +33,11 @@ static const Rule rules[] = {
 	 *	WM_CLASS(STRING) = instance, class
 	 *	WM_NAME(STRING) = title
 	 */
-	/* class      instance    title       tags mask     isfloating   monitor */
-	{ "Gimp",     NULL,       NULL,       0,            1,           -1 },
-	{ "Firefox",  NULL,       NULL,       1 << 8,       0,           -1 },
+	/* class			instance    title       tags mask     isfloating   monitor */
+	{ "Gimp",			NULL,       NULL,       0,            1,           -1 },
+	{ "Firefox",		NULL,       NULL,       1 << 8,       0,           -1 },
+	{ "Google-chrome",	NULL,       NULL,       1 << 7,       0,           0  },
+	{ "Code",			NULL,       NULL,       1 << 4,       0,           0  },
 };
 
 /* layout(s) */
@@ -52,7 +54,7 @@ static const Layout layouts[] = {
 };
 
 /* key definitions */
-#define MODKEY Mod1Mask
+#define MODKEY Mod4Mask
 #define TAGKEYS(KEY,TAG) \
 	{ MODKEY,                       KEY,      view,           {.ui = 1 << TAG} }, \
 	{ MODKEY|ControlMask,           KEY,      toggleview,     {.ui = 1 << TAG} }, \
